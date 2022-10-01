640fae5cb6d619881eb43ef27fb763d6d55ca0e5
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-10-01 14:49:07 -0400

    bonus solve if callback function is not passed

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-
-const character = newImage('assets/green-character/static.gif')
+const character = newImage("assets/green-character/static.gif");
 
 //function to change character image with respect to the direction in which it's moving
 
 function handleDirectionChange(direction) {
-    if(direction === null){
-        character.src = 'assets/green-character/static.gif'
-    }
-    if(direction === 'west'){
-        character.src = 'assets/green-character/west.gif'
-    }
-    if(direction === 'north'){
-        character.src = 'assets/green-character/north.gif'
-    }
-    if(direction === 'east'){
-        character.src = 'assets/green-character/east.gif'
-    }
-    if(direction === 'south'){
-        character.src = 'assets/green-character/south.gif'
-    }
+  if (direction === null) {
+    character.src = "assets/green-character/static.gif";
+  }
+  if (direction === "west") {
+    character.src = "assets/green-character/west.gif";
+  }
+  if (direction === "north") {
+    character.src = "assets/green-character/north.gif";
+  }
+  if (direction === "east") {
+    character.src = "assets/green-character/east.gif";
+  }
+  if (direction === "south") {
+    character.src = "assets/green-character/south.gif";
+  }
 }
 
-
 // set character to different positions based on arrow keys pressed, using event handler
-move(character).withArrowKeys(100, 250, handleDirectionChange)
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+// move(newImage("assets/tree.png")).withArrowKeys(200,450);
 
-
-
-
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
--- move.js
+++ move.js
@@ -1,69 +1,71 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
+
+  function moveWithArrowKeys(left, bottom, directionChange) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + "px";
+    element.style.bottom = y + "px";
+
+    // change value of x and y based on direction
+    function moveCharacter() {
+      if (direction === "west") {
+        x -= 1;
+      }
+      if (direction === "north") {
+        y += 1;
+      }
+      if (direction === "east") {
+        x += 1;
+      }
+      if (direction === "south") {
+        y -= 1;
+      }
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
     }
 
-    function moveWithArrowKeys(left, bottom, directionChange){
-        let direction = null;
-        let x = left;
-        let y = bottom;
-    
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
-        
-        // change value of x and y based on direction
-        function moveCharacter(){ 
-            if(direction === 'west'){
-                x-=1
-            }
-            if(direction === 'north'){
-                y+=1
-            }
-            if(direction === 'east'){
-                x+=1
-            }
-            if(direction === 'south'){
-                y-=1
-            }
-            element.style.left = x + 'px'
-            element.style.bottom = y + 'px'
-        }
-        
-        // call the function passed every 1 ms
-        setInterval(moveCharacter, 1)
-        
-        // event listener when key is pressed
-        document.addEventListener('keydown', function(e){
-            if(e.repeat) return;
-        
-            if(e.key === 'ArrowLeft'){
-                direction = 'west'
-            }
-            if(e.key === 'ArrowUp'){
-                direction = 'north'
-            }
-            if(e.key === 'ArrowRight'){
-                direction = 'east'
-            }
-            if(e.key === 'ArrowDown'){
-                direction = 'south'
-            }
-            directionChange(direction)
-        })
-        
-        // event listener when key is released 
-        document.addEventListener('keyup', function(e){
-            direction = null
-            directionChange(direction)
-        })
-    }
-    
+    // call the function passed every 1 ms
+    setInterval(moveCharacter, 1);
 
-    return {
-        to: moveToCoordinates,
-        withArrowKeys: moveWithArrowKeys
-    }
+    // event listener when key is pressed
+    document.addEventListener("keydown", function (e) {
+      if (e.repeat) return;
+
+      if (e.key === "ArrowLeft") {
+        direction = "west";
+      }
+      if (e.key === "ArrowUp") {
+        direction = "north";
+      }
+      if (e.key === "ArrowRight") {
+        direction = "east";
+      }
+      if (e.key === "ArrowDown") {
+        direction = "south";
+      }
+      if (typeof(directionChange) === 'function') {
+        directionChange(direction);
+      }
+        
+    });
+
+    // event listener when key is released
+    document.addEventListener("keyup", function (e) {
+      direction = null;
+      directionChange(direction);
+    });
+  }
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
